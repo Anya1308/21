6c9a271205de07ccb0ef0d3b6ea0d546d0431988
Anya1308 <96870990+Anya1308@users.noreply.github.com>
2022-06-08 20:10:36 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var shark, sharkImg
+var fish1, fish1Img
+var fish2, fish2Img
+var fish3, fiah3Img
+var sea, seaImg
+
+function preload(){
+ sharkImg.loadImage("shark.png");
+ fish1Img.loadImage("fish_ 1.jpg");
+ fish2Img.loadImage("fish_2.png");
+ fish3Img.loadImage("fish_3,png");
+ seaImg.loadImage("sea.jpg")
+}
+
+function setup() {
+createCanvas(600,600);
+shark = createSprite(100,300,20,40);
+shark.addImage()
+}
+
+function draw() {
+ 
+}
